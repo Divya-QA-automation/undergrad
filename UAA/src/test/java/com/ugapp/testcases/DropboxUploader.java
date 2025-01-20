f018711048800e711bb9c7e54767e3de39f4d341
divya <divya@DESKTOP-4IOJORV>
2025-01-20 16:15:35 +0530

    Latest push >> Includes NDG/VUS flow

--- UAA/src/test/java/com/ugapp/testcases/DropboxUploader.java
+++ UAA/src/test/java/com/ugapp/testcases/DropboxUploader.java
@@ -1,69 +1,69 @@
-package com.ugapp.testcases;
-
-import com.dropbox.core.DbxException;
-import com.dropbox.core.DbxRequestConfig;
-import com.dropbox.core.v2.DbxClientV2;
-import com.dropbox.core.v2.files.FileMetadata;
-import com.dropbox.core.v2.files.WriteMode;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-public class DropboxUploader {
-
-    // Dropbox Access Token
-//    private static final String ACCESS_TOKEN = "sl.CDPqcd6Oz1t-5XtCxF_fnuKCoDIIig2VXc5u9aQpSrMjRYeUXEUJrWvCfKJlZqkftYhMsQrhqsO2BiY9pH9naN07VgyPNmFvEcnZZp_4uxzBBhtlBtSu9xtuww2bTK8eRwpx8XDBijEk"; 
-
-    public static void main(String[] args) {
-        // Initialize Dropbox client
-        DbxRequestConfig config = DbxRequestConfig.newBuilder("java-dropbox-example").build();
-        DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
-
-        // File to upload
-        String localFilePath = "./target/surefire-reports/html/extent.html"; // Local file path
-        String dropboxFilePath = "/test_results/extent.html"; // Dropbox path
-
-        try {
-            // Ensure folder exists
-            ensureFolderExists(client, "/test_results");
-
-            // Upload the file to Dropbox
-            uploadFile(client, localFilePath, dropboxFilePath);
-        } catch (IOException | DbxException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void ensureFolderExists(DbxClientV2 client, String folderPath) throws DbxException {
-        try {
-            client.files().getMetadata(folderPath);
-        } catch (DbxException e) {
-            // If folder does not exist, create it
-            client.files().createFolderV2(folderPath);
-            System.out.println("Created folder: " + folderPath);
-        }
-    }
-
-    private static void uploadFile(DbxClientV2 client, String localFilePath, String dropboxFilePath) throws IOException, DbxException {
-        FileInputStream inputStream = null;
-        try {
-            // Open the local file
-            inputStream = new FileInputStream(localFilePath);
-
-            // Upload the file to Dropbox
-            FileMetadata metadata = client.files().uploadBuilder(dropboxFilePath)
-                    .withMode(WriteMode.OVERWRITE)
-                    .uploadAndFinish(inputStream);
-
-            System.out.println("File uploaded to Dropbox: " + metadata.getPathLower());
-
-        } catch (FileNotFoundException e) {
-            System.err.println("File not found: " + localFilePath);
-        } finally {
-            if (inputStream != null) {
-                inputStream.close();
-            }
-        }
-    }
-}
+//package com.ugapp.testcases;
+//
+//import com.dropbox.core.DbxException;
+//import com.dropbox.core.DbxRequestConfig;
+//import com.dropbox.core.v2.DbxClientV2;
+//import com.dropbox.core.v2.files.FileMetadata;
+//import com.dropbox.core.v2.files.WriteMode;
+//
+//import java.io.FileInputStream;
+//import java.io.FileNotFoundException;
+//import java.io.IOException;
+//
+//public class DropboxUploader {
+//
+//    // Dropbox Access Token
+////    private static final String ACCESS_TOKEN = "sl.CDPqcd6Oz1t-5XtCxF_fnuKCoDIIig2VXc5u9aQpSrMjRYeUXEUJrWvCfKJlZqkftYhMsQrhqsO2BiY9pH9naN07VgyPNmFvEcnZZp_4uxzBBhtlBtSu9xtuww2bTK8eRwpx8XDBijEk"; 
+//
+//    public static void main(String[] args) {
+//        // Initialize Dropbox client
+//        DbxRequestConfig config = DbxRequestConfig.newBuilder("java-dropbox-example").build();
+//        DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
+//
+//        // File to upload
+//        String localFilePath = "./target/surefire-reports/html/extent.html"; // Local file path
+//        String dropboxFilePath = "/test_results/extent.html"; // Dropbox path
+//
+//        try {
+//            // Ensure folder exists
+//            ensureFolderExists(client, "/test_results");
+//
+//            // Upload the file to Dropbox
+//            uploadFile(client, localFilePath, dropboxFilePath);
+//        } catch (IOException | DbxException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    private static void ensureFolderExists(DbxClientV2 client, String folderPath) throws DbxException {
+//        try {
+//            client.files().getMetadata(folderPath);
+//        } catch (DbxException e) {
+//            // If folder does not exist, create it
+//            client.files().createFolderV2(folderPath);
+//            System.out.println("Created folder: " + folderPath);
+//        }
+//    }
+//
+//    private static void uploadFile(DbxClientV2 client, String localFilePath, String dropboxFilePath) throws IOException, DbxException {
+//        FileInputStream inputStream = null;
+//        try {
+//            // Open the local file
+//            inputStream = new FileInputStream(localFilePath);
+//
+//            // Upload the file to Dropbox
+//            FileMetadata metadata = client.files().uploadBuilder(dropboxFilePath)
+//                    .withMode(WriteMode.OVERWRITE)
+//                    .uploadAndFinish(inputStream);
+//
+//            System.out.println("File uploaded to Dropbox: " + metadata.getPathLower());
+//
+//        } catch (FileNotFoundException e) {
+//            System.err.println("File not found: " + localFilePath);
+//        } finally {
+//            if (inputStream != null) {
+//                inputStream.close();
+//            }
+//        }
+//    }
+//}
--- UAA/src/test/java/com/ugapp/testcases/SlackIntegration.java
+++ UAA/src/test/java/com/ugapp/testcases/SlackIntegration.java
@@ -1,82 +1,82 @@
-package com.ugapp.testcases;
-
-import org.testng.ITestContext;
-import org.testng.ISuite;
-import org.testng.annotations.Test;
-
-import com.slack.api.Slack;
-import com.slack.api.methods.MethodsClient;
-import com.slack.api.methods.request.chat.ChatPostMessageRequest;
-import com.slack.api.methods.response.chat.ChatPostMessageResponse;
-import com.slack.api.model.Message;
-import com.ugapp.listeners.CustomListeners;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-public class SlackIntegration {
-
-	String TOKEN = "xoxb-7859208786195-8188397499396-b71pWMkIjqn7k1k9TV0DsZ77";
-	String CHANNEL = "C07RBQ3DTFW";
-	 int passedCount = 0;
-	 int failedCount = 0;
-	 int totalTestsCount = 0;
-
-
-	public  void main(String[] args) 
-	{
-		sendMessageToSlack(0, 0, 0);
-	}
-
-	public void testSlackIntegration() 
-	{
-		// Create an instance of CustomListeners to access its methods
-		CustomListeners customListeners = new CustomListeners();
-		customListeners.onFinish((ISuite)null); // Call onFinish to calculate percentages
-
-		// Retrieve percentages from CustomListeners
-		int passPercentage = customListeners.getPassPercentage();
-		int failPercentage = customListeners.getFailPercentage();
-		int totalPercentage = customListeners.getTotalPercentage();
-	}
-
-
-	public  void sendMessageToSlack(int passPercentage, int failPercentage, int totalPercentage) 
-	{
-		Slack slack = Slack.getInstance();
-		MethodsClient methods = slack.methods(TOKEN);
-		
-	
-		
-		
-		
-		
-
-		String msg = "UnderGrad QA Automation report:\nScript execution date: " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("d MMM Y"))
-				+ "\n\nTotal number of test cases: " + (passPercentage + failPercentage)
-				+ "\n Passed: " + passedCount + "%"
-				+ "\n Failed: " + failedCount + "%"
-				+ "\n TOTAL: " + totalTestsCount + "%" 
-				+ "\n\nLink to the detailed report: <https://www.dropbox.com/scl/fo/2g8h3kg80rghcwde83e3u/ACcEh7bQ3C9mbehpyRPrQPI?rlkey=8w1uluyl6xuj5mgiz1uest0kz&st=ii1p4l6e&dl=0|View Test Report>";
-
-		ChatPostMessageRequest request = ChatPostMessageRequest.builder()
-				.channel(CHANNEL)
-				.text(msg)
-				.iconEmoji(":twice:")
-				.build();
-
-
-		// Send the message
-		try {
-			ChatPostMessageResponse response = methods.chatPostMessage(request);
-			if (response.isOk()) {
-				Message sentMessage = response.getMessage();
-				System.out.println("Message sent: " + sentMessage.getText());
-			} else {
-				System.out.println("Failed to send message: " + response.getError());
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-}
+//package com.ugapp.testcases;
+//
+//import org.testng.ITestContext;
+//import org.testng.ISuite;
+//import org.testng.annotations.Test;
+//
+//import com.slack.api.Slack;
+//import com.slack.api.methods.MethodsClient;
+//import com.slack.api.methods.request.chat.ChatPostMessageRequest;
+//import com.slack.api.methods.response.chat.ChatPostMessageResponse;
+//import com.slack.api.model.Message;
+//import com.ugapp.listeners.CustomListeners;
+//
+//import java.time.LocalDateTime;
+//import java.time.format.DateTimeFormatter;
+//
+//public class SlackIntegration {
+//
+//	String TOKEN = "xoxb-7859208786195-8188397499396-b71pWMkIjqn7k1k9TV0DsZ77";
+//	String CHANNEL = "C07RBQ3DTFW";
+//	 int passedCount = 0;
+//	 int failedCount = 0;
+//	 int totalTestsCount = 0;
+//
+//
+//	public  void main(String[] args) 
+//	{
+//		sendMessageToSlack(0, 0, 0);
+//	}
+//
+//	public void testSlackIntegration() 
+//	{
+//		// Create an instance of CustomListeners to access its methods
+//		CustomListeners customListeners = new CustomListeners();
+//		customListeners.onFinish((ISuite)null); // Call onFinish to calculate percentages
+//
+//		// Retrieve percentages from CustomListeners
+//		int passPercentage = customListeners.getPassPercentage();
+//		int failPercentage = customListeners.getFailPercentage();
+//		int totalPercentage = customListeners.getTotalPercentage();
+//	}
+//
+//
+//	public  void sendMessageToSlack(int passPercentage, int failPercentage, int totalPercentage) 
+//	{
+//		Slack slack = Slack.getInstance();
+//		MethodsClient methods = slack.methods(TOKEN);
+//		
+//	
+//		
+//		
+//		
+//		
+//
+//		String msg = "UnderGrad QA Automation report:\nScript execution date: " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("d MMM Y"))
+//				+ "\n\nTotal number of test cases: " + (passPercentage + failPercentage)
+//				+ "\n Passed: " + passedCount + "%"
+//				+ "\n Failed: " + failedCount + "%"
+//				+ "\n TOTAL: " + totalTestsCount + "%" 
+//				+ "\n\nLink to the detailed report: <https://www.dropbox.com/scl/fo/2g8h3kg80rghcwde83e3u/ACcEh7bQ3C9mbehpyRPrQPI?rlkey=8w1uluyl6xuj5mgiz1uest0kz&st=ii1p4l6e&dl=0|View Test Report>";
+//
+//		ChatPostMessageRequest request = ChatPostMessageRequest.builder()
+//				.channel(CHANNEL)
+//				.text(msg)
+//				.iconEmoji(":twice:")
+//				.build();
+//
+//
+//		// Send the message
+//		try {
+//			ChatPostMessageResponse response = methods.chatPostMessage(request);
+//			if (response.isOk()) {
+//				Message sentMessage = response.getMessage();
+//				System.out.println("Message sent: " + sentMessage.getText());
+//			} else {
+//				System.out.println("Failed to send message: " + response.getError());
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//}
